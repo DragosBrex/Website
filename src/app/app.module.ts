630d2bf8abb1e442deb8742a2ed0a717f4956c1c
Dragos Brisc <dragosbrex@gmail.com>
2023-03-14 22:39:08 +0200

    second commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    ProductPageComponent,
+    ProductComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductPageComponent },
+      { path: 'login', component: LoginPageComponent },
+      { path: 'signup', component: SignupPageComponent },
+      { path: 'products', component: ProductPageComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
